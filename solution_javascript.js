ce21756cf138d6c846268124de6a0755160a3cda
Eric <eric@localhost.localdomain>
2020-07-10 14:38:46 -0400

    Added JS implementation, comments to both

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,14 +1,70 @@
 class EventSourcer {
+  /*
+    Eric Corbu
+    corb0400@mylaurier.ca
+
+    I haven't used event sourcing before so I'm not sure if this is implemented properly, but it passes the given tests with npm test'
+    JavaScript Implementation (same in functionality as Python implementation)
+  */
+    
   constructor() {
+    // initializing currentEvent as -1 seems strange, but the first operation should be at index 0
     this.value = 0;
+    this.currentEvent = -1;
+    this.events = [];
   }
 
-  add(num) {}
-  subtract(num) {}
-  undo() {}
-  redo() {}
-  bulk_undo(num) {}
-  bulk_redo(num) {}
+  add(num) {
+    // adds value to total pushes event to the list, and increments the current event
+
+    this.value += num;
+    this.events.push(num);
+    this.currentEvent++;
+  }
+
+  subtract(num) {
+    // subtracts value from total, pushes event to the list (subtration event is represented by a nevative integer), increments current event
+
+    this.value -= num;
+    this.events.push(-num);
+    this.currentEvent ++;
+  }
+
+  undo() {
+    // checks that there has been an event pushed, so that undo is possible
+
+    if (this.currentEvent >= 0) {
+      // subtracts value of last event from total
+      this.value -= this.events[this.currentEvent];
+      // moves currentEvent back one place
+      this.currentEvent -= 1;
+    }
+  }
+
+  redo() {
+    // checks that the currentEvent is not the last event, so that redo is possible
+    if (this.events.length -1 > this.currentEvent) {
+      // moves currentEvent forward one place
+      this.currentEvent++;
+      // adds value of last event to total
+      this.value += this.events[this.currentEvent];
+     
+    }
+  }
+
+  bulk_undo(num) {
+    // calls undo function for number of steps
+    for (let i = 0; i < num; i++) {
+      this.undo();
+    }
+  }
+
+  bulk_redo(num) {
+    // calls redo function for number of steps
+    for (let i = 0; i < num; i++){
+      this.redo();
+    }
+  }
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
--- solution_python.py
+++ solution_python.py
@@ -1,45 +1,61 @@
 class EventSourcer():
     # Do not change the signature of any functions
-
-    # Eric Corbu
-    # I haven't used event sourcing before so I'm not sure if this is implemented properly, however it passes the given 'test_python_solution.py'
-
-
+    """
+        Eric Corbu
+        corb0400@mylaurier.ca
+        
+        I haven't used event sourcing before so I'm not sure if this is implemented properly, however it passes the given 'test_python_solution.py'
+        Python Implementation (works the same as JS implementation)
+    """
     def __init__(self):
+        # initializing currentEvent as -1 seems strange, but the first operation should be at index 0
+        
         self.value = 0
         self.currentEvent = -1
         self.events = []
 
     def add(self, num):
+        # adds value to total pushes event to the list, and increments the current event
+        
         self.value += num
         self.events.append(num)
         self.currentEvent += 1
         pass
 
     def subtract(self, num):
-        self.value -=num
+        # subtracts value from total, pushes event to the list (subtration event is represented by a nevative integer), increments current event
+        
+        self.value -= num
         self.events.append(-num)
         self.currentEvent += 1
         pass
 
     def undo(self):
+        #checks that there has been an event pushed, so that undo is possible
         if self.currentEvent >= 0:
+            # subtracts value of last event from total
             self.value -= self.events[self.currentEvent]
+            # moves currentEvent back one place
             self.currentEvent -= 1            
         pass
 
     def redo(self):
+        # checks that the currentEvent is not the last event, so that redo is possible
         if len(self.events) -1 > self.currentEvent:
+            # moves currentEvent forward one place
             self.currentEvent += 1
+            # adds value of last event to total
             self.value += self.events[self.currentEvent]
         pass
 
     def bulk_undo(self, steps):
+        # calls undo function for number of steps
         for _ in range(steps):
             self.undo()
         pass
 
     def bulk_redo(self, steps):
+        # calls redo function for number of steps
         for _ in range(steps):
             self.redo()
         pass
